20968c2232cc55088e1472c508573ff186bd3e18
Davit Karapetyan <davit207karapetyan@gmail.com>
2022-10-20 15:52:06 +0400

    Add project files.

---
+++ DavArtWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DavArtWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
